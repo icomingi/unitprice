ca4432f49dd7046b688bbac73c006aafd54d3d33
Alex Zhong <zhongzq04@gmail.com>
2014-01-14 23:27:53 +0800

    parst test

--- index.py
+++ index.py
@@ -1,12 +1,28 @@
 #-*- coding:utf-8 -*-
 import json
+import urllib
+from html_test import ProdInfoParser
+
+TEST_URL = r'http://www.yhd.com/ctg/s2/c33699-0/?ref=gl.1.1.1.[CatMenu_Site_100000003_24166].0.EHKgJo'
 
 def app(environ, start_response):
     status = '200 OK'
     headers = [('Content-type', 'application/json')]
     start_response(status, headers)
-    body = {'x': 1, 'y': 2, 'z': 3}
+    body = {'x': 11, 'y': 12, 'z': 13}
+    parser = ProdInfoParser()
+    parser.feed(getContent(TEST_URL))
+    parser.close()
+#    body = parser.output()
     return json.dumps(body)
+
+def getContent(url):
+    f = urllib.urlopen(url)
+    c = f.read()
+    f.close()
+    return c
+
+
 
 from bae.core.wsgi import WSGIApplication
 application = WSGIApplication(app)
