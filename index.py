ecfedcc29777ea54da3fadc5ade2396efe2cafaa
Alex Zhong <zhongzq04@gmail.com>
2014-01-14 22:52:25 +0800

    output json

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
 #-*- coding:utf-8 -*-
+import json
 
 def app(environ, start_response):
     status = '200 OK'
-    headers = [('Content-type', 'text/html')]
+    headers = [('Content-type', 'application/json')]
     start_response(status, headers)
-    body=["Welcome to Alex's Web App!\n", "激动人心"]
-    return body
+    body = {'x': 1, 'y': 2, 'z': 3}
+    return json.dumps(body)
 
 from bae.core.wsgi import WSGIApplication
 application = WSGIApplication(app)
